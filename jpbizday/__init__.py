ec0d9a658a34dec0a980121e45606dbd06387582
sig9org <sig9@sig9.org>
2020-02-03 02:08:34 +0900

    Update setup.cfg

--- jpbizday/__init__.py
+++ jpbizday/__init__.py
@@ -5,8 +5,3 @@ from .jpbizday import *
 """
 日本の営業日を扱うクラスです。 指定した日付が営業日か判定したり、指定年/月の営業日を取得したり、該当月の最初の/最後の営業日を取得したり、といったことが出来ます。
 """
-
-__version__ = '0.0.3'
-__license__ = 'MIT License'
-__author__ = 'sig9'
-__author_email__ = 'sig9@sig9.org'
